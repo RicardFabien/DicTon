0354c95c1f1270f5f81f4250da2b2e3b3092a850
farben <farben@farben-MS-7721>
2020-02-04 17:29:05 +0100

    Adding GUI prototype

--- DicTon/gui/windowManager.py
+++ DicTon/gui/windowManager.py
@@ -18,16 +18,25 @@ class windowManager:
         rootWindow = rootWindow
         
         
-        
         rootWindow.title("DicTon")
+        rootWindow.geometry("480x360")
+        rootWindow.minsize(480,360)
+        rootWindow.config(background = "#dfff80")
         
-        menubar = Menu(self.rootWindow)
+        button = Button(rootWindow, text = "Soumettre dictée")
+        button.pack(side = BOTTOM)
+        
+        
+        menubar = Menu(rootWindow)
 
         menu1 = Menu(menubar, tearoff=0)
         menu1.add_command(label="Choisir texte", command=alert)
         menubar.add_cascade(label="Fichier", menu=menu1)
         
+        
+        
         rootWindow.config(menu = menubar)
+        
         
         rootWindow.mainloop()
         
